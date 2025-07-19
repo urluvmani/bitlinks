603a8881db7f84fcd6eb3bf409d0d778b7ac0a1c
Hafiz Muhammad Abdul Rehman <abdulrehman90190@gmail.com>
2025-07-18 19:21:33 -0700

    s

--- app/about/page.js
+++ app/about/page.js
@@ -8,10 +8,10 @@ const AboutPage = () => {
       <div className="max-w-3xl mx-auto">
         <h1 className="text-4xl font-bold mb-6">About BitLinks</h1>
         <p className="text-lg mb-4">
-          <strong>BitLinks</strong> is a simple and fast URL shortener designed to turn long and complex web addresses into short, easy-to-remember links.
+          <strong>BitLinks</strong> is a simple and fast URL shortener designed to turn long and complex web addresses into short, easy to remember links.
         </p>
         <p className="text-lg mb-4">
-          Whether you're sharing links on social media, tracking click performance, or just want a cleaner way to share URLs — BitLinks helps you do it quickly and securely.
+          Whether you are sharing links on social media, tracking click performance, or just want a cleaner way to share URLs — BitLinks helps you do it quickly and securely.
         </p>
         <p className="text-lg mb-4">
           Built using <span className="font-semibold">Next.js</span>, <span className="font-semibold">MongoDB</span>, and deployed with <span className="font-semibold">Vercel</span>, this app ensures performance, reliability, and scalability.
